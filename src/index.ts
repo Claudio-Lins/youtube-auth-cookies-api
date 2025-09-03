ec604e0a9e272bc19d4e78c78baedf39b6d2dce6
Guido Cerqueira <euguidocerqueira@gmail.com>
2025-09-03 09:38:26 -0300

    implement auth jwt

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { prisma } from './prisma.js';
 import router from './routes.js';
+import cors from 'cors'
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors())
 
 app.use(router)
 
