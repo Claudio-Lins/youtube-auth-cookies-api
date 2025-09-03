2622dbbbb638f2d054e7b067946efe49050b5f17
Guido Cerqueira <euguidocerqueira@gmail.com>
2025-09-03 09:28:51 -0300

    implement auth jwt

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { prisma } from "./prisma.js";
 import { compare } from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
@@ -23,15 +24,42 @@ router.post('/login', async (req, res) => {
 		return res.status(401).json({ message: 'Invalid email or password' });
 	}
 
-	// Implement cookie-based authentication
+	const token = jwt.sign({ id: user.id }, 'UmaSenhaSuperSecreta', { expiresIn: '1h' });
 
-	res.json(user);
+	res.json({ user, token });
 });
 
 
 router.get('/profile', async (req, res) => {
-	const user = await prisma.user.findFirst();
-	res.json(user);
+	const { authorization } = req.headers;
+
+	if (!authorization) {
+		return res.status(401).json({ message: 'Unauthorized' });
+	}
+
+	const token = authorization.split(' ')[1];
+
+	if (!token) {
+		return res.status(401).json({ message: 'Unauthorized' });
+	}
+
+	try {
+		const decoded = jwt.verify(token, 'UmaSenhaSuperSecreta');
+
+		const user = await prisma.user.findFirst({
+			where: {
+				id: (decoded as any).id
+			}
+		});
+
+		if (!user) {
+			return res.status(401).json({ message: 'Unauthorized' });
+		}
+		
+		res.json(user);
+	} catch (error) {
+		return res.status(401).json({ message: 'Unauthorized' });
+	}
 });
 
 export default router;
