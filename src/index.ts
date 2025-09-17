381725fb272ee640bb11b3c0600b0a5b203d8bc1
Guido Cerqueira <euguidocerqueira@gmail.com>
2025-09-17 18:52:34 -0300

    implement cookies auth

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import router from './routes.js';
 import cors from 'cors'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cookieParser());
+app.use(cors({
+	origin: true,
+	credentials: true
+}))
 
 app.use(router)
 
--- src/routes.ts
+++ src/routes.ts
@@ -26,18 +26,23 @@ router.post('/login', async (req, res) => {
 
 	const token = jwt.sign({ id: user.id }, 'UmaSenhaSuperSecreta', { expiresIn: '1h' });
 
-	res.json({ user, token });
+	const isProd = process.env.NODE_ENV === 'production'
+
+	res.cookie('token_youtube', token, {
+		httpOnly: true,
+		secure: isProd, // true em produção com HTTPS
+		sameSite: isProd ? 'none' : 'lax', // 'none' em produção com HTTPS
+		maxAge: 60 * 60 * 1000, // 1 hora
+		path: '/',
+		domain: isProd ? '.seu-dominio.com' : 'localhost' // ajuste conforme necessário
+	})
+
+	res.json({ user });
 });
 
 
 router.get('/profile', async (req, res) => {
-	const { authorization } = req.headers;
-
-	if (!authorization) {
-		return res.status(401).json({ message: 'Unauthorized' });
-	}
-
-	const token = authorization.split(' ')[1];
+	const token = req.cookies['token_youtube']
 
 	if (!token) {
 		return res.status(401).json({ message: 'Unauthorized' });
@@ -60,6 +65,11 @@ router.get('/profile', async (req, res) => {
 	} catch (error) {
 		return res.status(401).json({ message: 'Unauthorized' });
 	}
+});
+
+router.post('/logout', (req, res) => {
+	res.clearCookie('token_youtube')
+	res.json({ message: 'Logged out successfully' });
 });
 
 export default router;
